069eab0a3ff95fe64c3b66f97882c6da697fdf9e
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-19 12:32:59 -0600

    provider factory sequence

--- spec/factories/providers.rb
+++ spec/factories/providers.rb
@@ -1,15 +1,60 @@
 FactoryGirl.define do
 
-  sequence(:date, (10..30).cycle) do |n|
-    "2016-1-#{n}"
-  end
-
   sequence(:provider_name, (["AOL", "Gmail", "Yahoo", "Hotmail"].cycle)) do |n|
     "#{n}"
   end
 
+  sequence(:blocks) do |n|
+    rand(10)
+  end
+
+  sequence(:bounces) do |n|
+    rand(15)
+  end
+
+  sequence(:clicks) do |n|
+    rand(1000)
+  end
+
+  sequence(:deferred) do |n|
+    rand(100)
+  end
+
+  sequence(:delivered) do |n|
+    rand(2000)
+  end
+
+  sequence(:drops) do |n|
+    rand(20)
+  end
+
+  sequence(:opens) do |n|
+    rand(400)
+  end
+
+  sequence(:spam_reports) do |n|
+    rand(10)
+  end
+
+  sequence(:unique_clicks) do |n|
+    rand(100)
+  end
+
+  sequence(:unique_opens) do |n|
+    rand(250)
+  end
+  
   factory :provider do
-    date
     provider_name
+    blocks
+    bounces
+    clicks
+    deferred
+    delivered
+    drops
+    opens
+    spam_reports
+    unique_clicks
+    unique_opens
   end
 end
--- spec/requests/api/v1/provider_request_spec.rb
+++ spec/requests/api/v1/provider_request_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 describe "provider endpoint" do
   before(:each) do
-    create_list(:provider, 10)
+    create_list(:provider, 4, date: "2016-1-01")
+    create_list(:provider, 4, date: "2016-1-02")
+    create_list(:provider, 4, date: "2016-1-03")
+    create_list(:provider, 4, date: "2016-1-04")
   end
 
   context "POST poviders" do
