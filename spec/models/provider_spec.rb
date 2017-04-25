d6beda82e0dd730842c6a5bff986ff618f262456
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-25 08:39:30 -0600

    validation testing

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe Provider, type: :model do
     expect(utc).to eq(1483401600000)
     end
   end
+
+  context 'validation testing' do
+    it 'should save with all data' do
+      prov = build(:provider, date: "2016-1-01")
+
+      prov.save
+
+      expect(Provider.all.first.date).to eq(prov.date)
+    end
+    it 'should not save without a date' do
+      prov = build(:provider)
+
+      provider = prov.save
+
+      expect(provider).to eq(false)
+      expect(Provider.first).to eq(nil)
+    end
+
+    
+  end
 end
