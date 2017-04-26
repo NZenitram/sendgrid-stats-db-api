6e9f07eae7d9806df46cd77dacc7813daa6f0c84
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-26 12:36:12 -0600

    fixed code climate

--- app/controllers/api/v1/global_stats_controller.rb
+++ app/controllers/api/v1/global_stats_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GlobalStatsController < ApplicationController
   protect_from_forgery with: :null_session
-  skip_before_filter  :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
 
   def index
     render json: GlobalStat.all
